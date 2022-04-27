8ea822a9d79fb8eee6bbfbce3a1a81f3e3618fc2
Hazal Sendogan <hazal.sendogan@maarifa.com.tr>
2022-04-27 19:14:30 +0300

    Project created

---
+++ PeriodicTable/PeriodicTable/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PeriodicTable.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
